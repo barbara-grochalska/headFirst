0f7331cdbce481a356cb58fbda7f459f08c8b789
Barbara Grochalska <b.grochalska@gmail.com>
2018-09-25 16:14:31 +0200

    loop while added

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ var location1 = 3;
 var location2 = 4;
 var location = 5;
 
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while (isSunk ==false) {
+    guess = prompt("Are you ready? (just give the number from 0-6):");
+    
+}
